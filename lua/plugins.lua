5a702d4d781749793f90410043866d052433b139
gravitys <gravityshark@artix.localdomain>
2023-10-04 01:52:03 +0800

    lazy treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,7 +24,7 @@ return {
 	},
 	-- }}}
 
-	-- mini.nvim {{{}}}
+	-- mini.nvim {{{
 	{
 		'echasnovski/mini.pairs',
 		version = false,
@@ -40,6 +40,15 @@ return {
 			symbol = '▎',
 		},
 	},
+
+	-- "gc" to comment visual regions/lines
+	{
+		'echasnovski/mini.comment',
+		version = false,
+		keys = { 'gc', 'gcc' },
+		opts = {},
+	},
+	-- }}}
 
 	-- Git related plugins
 	-- 'tpope/vim-fugitive',
@@ -155,14 +164,6 @@ return {
 		},
 	},
 
-	-- "gc" to comment visual regions/lines
-	{
-		'echasnovski/mini.comment',
-		version = false,
-		keys = { 'gc', 'gcc' },
-		opts = {},
-	},
-
 	-- Fuzzy Finder (files, lsp, etc)
 	{
 		'nvim-telescope/telescope.nvim',
@@ -204,9 +205,89 @@ return {
 	{
 		-- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
+		event = { 'BufReadPost', 'BufNewFile' },
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-textobjects',
 		},
+		config = function()
+			vim.defer_fn(function()
+				require('nvim-treesitter.configs').setup({
+					-- Add languages to be installed here that you want installed for treesitter
+					ensure_installed = {
+						'c',
+						'cpp',
+						'go',
+						'lua',
+						'python',
+						'rust',
+						'tsx',
+						'javascript',
+						'typescript',
+						'vimdoc',
+						'vim',
+					},
+
+					-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+					auto_install = false,
+
+					highlight = { enable = true },
+					indent = { enable = true },
+					incremental_selection = {
+						enable = true,
+						keymaps = {
+							init_selection = '<c-space>',
+							node_incremental = '<c-space>',
+							scope_incremental = '<c-s>',
+							node_decremental = '<M-space>',
+						},
+					},
+					textobjects = {
+						select = {
+							enable = true,
+							lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+							keymaps = {
+								-- You can use the capture groups defined in textobjects.scm
+								['aa'] = '@parameter.outer',
+								['ia'] = '@parameter.inner',
+								['af'] = '@function.outer',
+								['if'] = '@function.inner',
+								['ac'] = '@class.outer',
+								['ic'] = '@class.inner',
+							},
+						},
+						move = {
+							enable = true,
+							set_jumps = true, -- whether to set jumps in the jumplist
+							goto_next_start = {
+								[']m'] = '@function.outer',
+								[']]'] = '@class.outer',
+							},
+							goto_next_end = {
+								[']M'] = '@function.outer',
+								[']['] = '@class.outer',
+							},
+							goto_previous_start = {
+								['[m'] = '@function.outer',
+								['[['] = '@class.outer',
+							},
+							goto_previous_end = {
+								['[M'] = '@function.outer',
+								['[]'] = '@class.outer',
+							},
+						},
+						swap = {
+							enable = true,
+							swap_next = {
+								['<leader>a'] = '@parameter.inner',
+							},
+							swap_previous = {
+								['<leader>A'] = '@parameter.inner',
+							},
+						},
+					},
+				})
+			end, 0)
+		end,
 		build = ':TSUpdate',
 	},
 
