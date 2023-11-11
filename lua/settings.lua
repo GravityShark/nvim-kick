381093d949813f8aa04140623a2bacb249eb5a3d
gravitys <alternativegravitational@protonmail.com>
2023-11-11 15:37:25 +0800

    yeah

--- lua/settings.lua
+++ lua/settings.lua
@@ -35,21 +35,21 @@ vim.o.breakindent = true
 vim.o.completeopt = 'menuone,noselect'
 vim.opt.colorcolumn = '80'
 
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new vertical splits to right
+vim.opt.splitbelow = true     -- Put new windows below current
+vim.opt.splitright = true     -- Put new vertical splits to right
 
-vim.opt.expandtab = true -- Use spaces by default
-vim.opt.shiftwidth = 4 -- Set amount of space characters, when we press "<" or ">"
+vim.opt.expandtab = true      -- Use spaces by default
+vim.opt.shiftwidth = 4        -- Set amount of space characters, when we press "<" or ">"
 vim.opt.softtabstop = 4
-vim.opt.tabstop = 4 -- 1 tab equal 2 spaces
-vim.opt.smartindent = true -- Turn on smart indentation. See in the docs for more info
+vim.opt.tabstop = 4           -- 1 tab equal 2 spaces
+vim.opt.smartindent = true    -- Turn on smart indentation. See in the docs for more info
 
 vim.opt.foldmethod = 'marker' -- Good
 
-vim.opt.ignorecase = true -- Ignore case if all characters in lower case
-vim.opt.joinspaces = false -- Join multiple spaces in search
-vim.opt.smartcase = true -- When there is a one capital letter search for exact match
-vim.opt.showmatch = true -- Highlight search instances
+vim.opt.ignorecase = true     -- Ignore case if all characters in lower case
+vim.opt.joinspaces = false    -- Join multiple spaces in search
+vim.opt.smartcase = true      -- When there is a one capital letter search for exact match
+vim.opt.showmatch = true      -- Highlight search instances
 
 -- Undotree Layout
 vim.g.undotree_WindowLayout = 3
