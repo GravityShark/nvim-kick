fa83d5b84e2cb1453acf7d0470fb991a0168297c
gravitys <alternativegravitational@protonmail.com>
2024-03-25 00:35:08 +0800

    orging

--- lua/pluggers/org.lua
+++ lua/pluggers/org.lua
@@ -39,12 +39,16 @@ return {
                 'DONE',
                 'CANCEL',
             },
+            org_agenda_start_on_weekday = false,
+            org_agenda_min_height = 32,
+            org_agenda_skip_scheduled_if_done = true,
+            org_agenda_skip_deadline_if_done = true,
             mappings = {
                 org = {
                     org_cycle = false,
-                    org_global_cycle = false
-                }
-            }
+                    org_global_cycle = false,
+                },
+            },
         })
     end,
 }
