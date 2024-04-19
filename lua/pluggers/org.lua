791a0b833ab8f36e1d186d49cce8916d515b226e
gravitys <alternativegravitational@protonmail.com>
2024-04-19 14:37:14 +0800

    org shit

--- lua/pluggers/org.lua
+++ lua/pluggers/org.lua
@@ -7,16 +7,11 @@ return {
         org_default_notes_file = '~/Notes/refile.org',
         org_todo_keywords = {
             'TODO',
-            'NEXT',
-            'HOLD',
-            'WAIT',
-            'INC',
             '|',
             'DONE',
             'CANCEL',
         },
         org_agenda_start_on_weekday = false,
-        org_agenda_min_height = 32,
         org_agenda_skip_scheduled_if_done = true,
         org_agenda_skip_deadline_if_done = true,
         mappings = {
