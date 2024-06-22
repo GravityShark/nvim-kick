932af3940b91f7d32a7ede81a7591841b46c1663
Gravity <alternativegravitational@protonmail.com>
2024-06-22 23:52:25 +0800

    persistence keybindings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -141,6 +141,24 @@ return {
                 '<CMD>lua require("persistence").stop()<CR>',
                 desc = 'Session [p]ause',
             },
+            {
+                '<leader>ss',
+                '<CMD>lua require("persistence").save()<CR>',
+                desc = 'Session [s]save',
+            },
+            {
+                '<leader>sd',
+                function()
+                    local M = require('persistence')
+                    local sfile = M.get_last() or M.get_current()
+                    if sfile and vim.fn.filereadable(sfile) ~= 0 then
+                        -- vim.cmd('silent! source ' .. vim.fn.fnameescape(sfile))
+                        assert(os.remove(sfile))
+                        M.stop()
+                    end
+                end,
+                desc = 'Session [d]elete',
+            },
         },
         opts = {
             options = { 'buffers,curdir,folds,tabpages,winpos,winsize' },
