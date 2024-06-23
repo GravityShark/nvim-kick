031459d59cda3f7d11b19e3c153247e1b1625f8c
Gravity <alternativegravitational@protonmail.com>
2024-06-23 15:48:05 +0800

    typst gaming

--- lua/pluggers/lspconfig.lua
+++ lua/pluggers/lspconfig.lua
@@ -114,27 +114,14 @@ for server_name, server in pairs(ensure.servers) do
     -- by the server configuration above. Useful when disabling
     -- certain features of an LSP (for example, turning off formatting for tsserver)
 
-    local server_filetypes = require('lspconfig/configs')['tsserver'].filetypes
-    table.insert(server_filetypes, 'some_filetype')
-    table.insert(server_filetypes, 'some_other_filetype')
+    server.capabilities = vim.tbl_deep_extend(
+        'force',
+        {},
+        capabilities,
+        server.capabilities or {}
+    )
 
-    require('lspconfig').tsserver.setup({
-        filetypes = server_filetypes,
-    })
-
-    server.capabilities =
-        vim.tbl_deep_extend('force', capabilities, server.capabilities or {})
-
-    if server.filetypes ~= {} then
-        server.filetypes = vim.tbl_deep_extend(
-            'force',
-            {},
-            lspconfig[server_name].default_config.filetypes or {},
-            server.filetypes or {}
-        )
-    end
-
-    lsp[server_name].setup(server)
+    require('lspconfig')[server_name].setup(server)
 end
 
 -- vim:foldmethod=marker:
