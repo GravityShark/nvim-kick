a3b86c2aa8b957eae70e2309f5201ed4af8ebd80
Gravity <alternativegravitational@protonmail.com>
2024-11-23 19:45:56 +0800

    yo

--- lua/pluggers/editor/mini.pairs.lua
+++ lua/pluggers/editor/mini.pairs.lua
@@ -4,18 +4,17 @@ return {
     config = function()
         local pairs = require('mini.pairs')
         pairs.setup({
-               modes = { insert = true, command = true, terminal = false },
-    -- skip autopair when next character is one of these
-    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-    -- skip autopair when the cursor is inside these treesitter nodes
-    skip_ts = { "string" },
-    -- skip autopair when next character is closing pair
-    -- and there are more closing pairs than opening pairs
-    skip_unbalanced = true,
-    -- better deal with markdown code blocks
-    markdown = true,
+    --            modes = { insert = true, command = true, terminal = false },
+    -- -- skip autopair when next character is one of these
+    -- skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- -- skip autopair when the cursor is inside these treesitter nodes
+    -- skip_ts = { "string" },
+    -- -- skip autopair when next character is closing pair
+    -- -- and there are more closing pairs than opening pairs
+    -- skip_unbalanced = true,
+    -- -- better deal with markdown code blocks
+    -- markdown = true,
         })
-         o
         vim.keymap.set('i', '<CR>', function()
             if
                 vim.fn.pumvisible() ~= 0
