6174249507e382f69989e8e921fb3e04bb9e7104
Gravity <alternativegravitational@protonmail.com>
2024-11-24 08:49:35 +0800

    back on track

--- lua/pluggers/editor/mini.pairs.lua
+++ lua/pluggers/editor/mini.pairs.lua
@@ -1,17 +1,18 @@
 return {
     'echasnovski/mini.pairs',
     event = 'VeryLazy',
-    config = function()
-        local pairs = require('mini.pairs')
-        pairs.setup()
-        vim.keymap.set('i', '<CR>', function()
-            if
-                vim.fn.pumvisible() ~= 0
-                and vim.fn.complete_info().selected == -1
-            then
-                return pairs.cr('<C-e><CR>')
-            end
-            return pairs.cr()
-        end, { silent = true, expr = true, replace_keycodes = true })
-    end,
+    opts = {},
+    -- config = function()
+    --     local pairs = require('mini.pairs')
+    --     pairs.setup()
+    --     vim.keymap.set('i', '<CR>', function()
+    --         if
+    --             vim.fn.pumvisible() ~= 0
+    --             and vim.fn.complete_info().selected == -1
+    --         then
+    --             return pairs.cr('<C-e><CR>')
+    --         end
+    --         return pairs.cr()
+    --     end, { silent = true, expr = true, replace_keycodes = true })
+    -- end,
 }
