f90d7ee9b40cdff066f785d04d85f10b2acd31f8
Gravity <alternativegravitational@protonmail.com>
2024-12-25 15:29:16 +0800

    spigg

--- lua/pluggers/main/lsp.lua
+++ lua/pluggers/main/lsp.lua
@@ -34,6 +34,14 @@ return { -- LSP Configuration (compatible with cmp, coq, and even neither!)
                     vim.lsp.buf.definition,
                     { buffer = ev.buf, desc = 'definition' }
                 )
+                vim.keymap.set(
+                    'n',
+                    '<leader>cl',
+                    vim.lsp.codelens.run()({
+                        buffer = ev.buf,
+                        desc = 'lens',
+                    })
+                )
                 vim.keymap.set('n', '<leader>ct', vim.lsp.buf.type_definition, {
                     buffer = ev.buf,
                     desc = 'type definition',
@@ -43,6 +51,16 @@ return { -- LSP Configuration (compatible with cmp, coq, and even neither!)
                     '<leader>cc',
                     vim.lsp.buf.code_action,
                     { buffer = ev.buf, desc = 'code actions' }
+                )
+                vim.keymap.set(
+                    'n',
+                    '<leader>cI',
+                    vim.lsp.inlay_hint.enable(
+                        not vim.lsp.inlay_hint.is_enabled()
+                    )({
+                        buffer = ev.buf,
+                        desc = 'inlay hints',
+                    })
                 )
                 vim.keymap.set('n', '<leader>cm', vim.lsp.buf.references, {
                     buffer = ev.buf,
