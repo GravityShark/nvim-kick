075c5928a74af9eabedf3f31bae667c7f4055f80
Gravity <alternativegravitational@protonmail.com>
2025-01-22 22:39:34 +0800

    yo

--- lua/pluggers/main/dap.lua
+++ lua/pluggers/main/dap.lua
@@ -1,1 +1,72 @@
-return {} -- TODO: add nvim-dap https://www.lazyvim.org/extras/dap/core
+return {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {
+        'rcarriga/nvim-dap-ui',
+        'nvim-neotest/nvim-nio',
+        -- virtual text for the debugger
+        {
+            'theHamsta/nvim-dap-virtual-text',
+            opts = {},
+        },
+    },
+    -- stylua: ignore
+    keys = {
+        { "<leader>d", desc = "debug" };
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
+        { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+        { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+        { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+        { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+        { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+        { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+        { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+        { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+        { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
+        { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+        { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+        { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+        { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+
+    config = function()
+        local dap = require('dap')
+        dap.adapters.lldb = {
+            type = 'executable',
+            command = '/run/current-system/sw/bin/lldb-dap',
+            name = 'lldb',
+        }
+        dap.configurations.c = {
+            {
+                name = 'Launch',
+                type = 'lldb',
+                request = 'launch',
+                program = function()
+                    return vim.fn.input(
+                        'Path to executable: ',
+                        vim.fn.getcwd() .. '/',
+                        'file'
+                    )
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false,
+                args = {},
+
+                -- 💀
+                -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+                --
+                --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+                --
+                -- Otherwise you might get the following error:
+                --
+                --    Error on launch: Failed to attach to the target process
+                --
+                -- But you should be aware of the implications:
+                -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+                -- runInTerminal = false,
+            },
+        }
+    end,
+} -- TODO: add nvim-dap https://www.lazyvim.org/extras/dap/core
