5d81cdd976dcfd9d5fb7fa2c5d89482496ebc3e8
Gravity <alternativegravitational@protonmail.com>
2025-04-24 20:29:24 +0800

    neovlim

--- lua/pluggers/extra/arrow.lua
+++ lua/pluggers/extra/arrow.lua
@@ -4,12 +4,13 @@ return {
     dependencies = { 'echasnovski/mini.icons' },
     opts = {
         show_icons = true,
+        always_show_path = true,
         leader_key = 'm', -- Recommended to be a single key
         buffer_leader_key = ',', -- Per Buffer Mappings
         -- index_keys = '123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
         -- index_keys = 'tnseriaoplfuwydhvmck',
         -- index_keys = 'tsradfweiouy',
-        index_keys = 'tsradpfwz',
+        index_keys = 'tsradbpfwzeiouy,./',
         save_key = 'git_root',
         mappings = {
             edit = 'c',
@@ -18,10 +19,10 @@ return {
             toggle = ' ', -- used as save if separate_save_and_remove is true
             open_vertical = 'v',
             open_horizontal = '-',
-            -- quit = 'q',
+            quit = nil,
             -- remove = 'x', -- only used if separate_save_and_remove is true
-            next_item = ']',
-            prev_item = '[',
+            next_item = nil,
+            prev_item = 'b',
         },
     },
 }
