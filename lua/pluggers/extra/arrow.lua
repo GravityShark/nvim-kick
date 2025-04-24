9843f18cc793812c7b57816cdf83f82955714e37
Gravity <alternativegravitational@protonmail.com>
2025-04-24 14:51:22 +0800

    arrow gaminng

--- lua/pluggers/extra/arrow.lua
+++ lua/pluggers/extra/arrow.lua
@@ -4,7 +4,9 @@ return {
     dependencies = { 'echasnovski/mini.icons' },
     opts = {
         show_icons = true,
-        leader_key = ';', -- Recommended to be a single key
-        buffer_leader_key = 'm', -- Per Buffer Mappings
+        leader_key = 'm', -- Recommended to be a single key
+        buffer_leader_key = ',', -- Per Buffer Mappings
+        -- index_keys = '123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
+        index_keys = 'tnseriaoplfuwydhvmck', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
     },
 }
