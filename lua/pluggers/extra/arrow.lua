0e80ba4115a3d7d33efad9d29ec7c6022dc40e5c
Gravity <alternativegravitational@protonmail.com>
2025-04-24 20:40:45 +0800

    hardtime make more hard

--- lua/pluggers/extra/arrow.lua
+++ lua/pluggers/extra/arrow.lua
@@ -5,15 +5,14 @@ return {
     opts = {
         show_icons = true,
         always_show_path = true,
-        leader_key = 'm', -- Recommended to be a single key
-        buffer_leader_key = ',', -- Per Buffer Mappings
-        -- index_keys = '123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
-        -- index_keys = 'tnseriaoplfuwydhvmck',
-        -- index_keys = 'tsradfweiouy',
+        -- hide_handbook = true,
+        -- hide_buffer_handbook = true,
+        leader_key = 'm',
+        buffer_leader_key = ',',
         index_keys = 'tsradcxzbpfw',
         save_key = 'git_root',
         mappings = {
-            toggle = ' ', -- used as save if separate_save_and_remove is true
+            toggle = ' ', -- <- this is a space btw
             delete_mode = 'k',
         },
     },
--- lua/pluggers/extra/hardtime.lua
+++ lua/pluggers/extra/hardtime.lua
@@ -3,7 +3,9 @@ return {
     lazy = false,
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     opts = {
-        max_time = 3000,
+        max_time = 5000,
+        timeout = false,
+        allow_different_key = false,
         disabled_keys = {
             ['<C-w>c'] = { 'n' },
         },
