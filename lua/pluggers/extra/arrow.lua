9a3cf8bb2da3bf5b7182108f489c26f9e3410c2e
Gravity <alternativegravitational@protonmail.com>
2025-04-24 15:06:22 +0800

    more arrow stuff

--- lua/pluggers/extra/arrow.lua
+++ lua/pluggers/extra/arrow.lua
@@ -7,6 +7,20 @@ return {
         leader_key = 'm', -- Recommended to be a single key
         buffer_leader_key = ',', -- Per Buffer Mappings
         -- index_keys = '123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
-        index_keys = 'tnseriaoplfuwydhvmck', -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
+        -- index_keys = 'tnseriaoplfuwydhvmck',
+        index_keys = 'tsradfw',
+        save_key = 'git_root',
+        mappings = {
+            edit = 'c',
+            delete_mode = 'x',
+            clear_all_items = 'C',
+            toggle = 'n', -- used as save if separate_save_and_remove is true
+            open_vertical = 'v',
+            open_horizontal = '-',
+            -- quit = 'q',
+            -- remove = 'x', -- only used if separate_save_and_remove is true
+            next_item = ']',
+            prev_item = '[',
+        },
     },
 }
