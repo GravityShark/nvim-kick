142ec24268ca3158d27de25e6cdf27c82c39573d
Gravity <alternativegravitational@protonmail.com>
2025-05-26 11:44:00 +0800

    ye

--- lua/floating_netrw.lua
+++ lua/floating_netrw.lua
@@ -11,9 +11,9 @@ function M.toggle()
 
     local ui = vim.api.nvim_list_uis()[1]
     local width = math.floor(ui.width * 0.8)
-    local height = math.floor(ui.height * 0.8) - 5
+    local height = math.floor((ui.height - 2) * 0.8)
     local col = math.floor((ui.width - width) / 2)
-    local row = math.floor((ui.height - height) / 2)
+    local row = math.floor(((ui.height - 2) - height) / 2)
 
     local opts = {
         relative = 'editor',
@@ -27,11 +27,10 @@ function M.toggle()
 
     if not state.buf or not vim.api.nvim_buf_is_valid(state.buf) then
         state.buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_option(state.buf, 'bufhidden', 'wipe')
     end
 
     state.win = vim.api.nvim_open_win(state.buf, true, opts)
-
-    vim.api.nvim_buf_set_option(state.buf, 'bufhidden', 'wipe')
 
     vim.cmd('lcd ' .. vim.fn.getcwd())
     vim.cmd('edit .')
