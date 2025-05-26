d0cd455470d5c23d50afa6011606bb2d19aae1ce
Gravity <alternativegravitational@protonmail.com>
2025-05-26 14:44:51 +0800

    finally

--- lua/pluggers/editor/mini.jump.lua
+++ lua/pluggers/editor/mini.jump.lua
@@ -8,15 +8,10 @@ return {
     },
     config = function(_, opts)
         require('mini.jump').setup(opts)
-        local search_hl =
-            vim.api.nvim_get_hl(0, { name = 'Search', link = false })
-        local minijump_hl =
-            vim.api.nvim_get_hl(0, { name = 'MiniJump', link = false })
 
-        -- Set MiniJumpSpot with same colors and extra styles
-        vim.api.nvim_set_hl(0, 'MiniJumpSpot', {
-            fg = minijump_hl.fg,
-            bg = search_hl.bg,
+        vim.api.nvim_set_hl(0, 'MiniJump', {
+            fg = vim.api.nvim_get_hl(0, { name = 'MiniJump', link = false }).fg,
+            bg = vim.api.nvim_get_hl(0, { name = 'Search', link = false }).bg,
             bold = true,
             underline = true,
         })
