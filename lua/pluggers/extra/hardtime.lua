13b9e0fa864e2e46b603c41f763dbac6da9a1c3d
Gravity <alternativegravitational@protonmail.com>
2025-05-26 17:43:35 +0800

    idk anymmore

--- lua/pluggers/extra/hardtime.lua
+++ lua/pluggers/extra/hardtime.lua
@@ -5,7 +5,7 @@ return {
     opts = {
         max_time = 5000,
         max_count = 2,
-        timeout = false,
+        timeout = 10000,
         resetting_keys = {},
         restricted_keys = {
             ['w'] = { 'n', 'x' },
@@ -15,12 +15,39 @@ return {
             ['b'] = { 'n', 'x' },
             ['B'] = { 'n', 'x' },
             ['J'] = { 'n', 'x' },
-            ['d'] = { 'n', 'x' },
             ['x'] = { 'n', 'x' },
             ['X'] = { 'n', 'x' },
             ['<'] = { 'n', 'x' },
             ['>'] = { 'n', 'x' },
             ['u'] = { 'n', 'x' },
+            ['<BS>'] = { 'i' },
+        },
+        hints = {
+            ['%D1[webWEB]'] = {
+                message = function()
+                    return 'Use f or t instead of spamming vro 💔'
+                end,
+                length = 1,
+            },
+
+            ['%D1[<>]'] = {
+                message = function()
+                    return 'Enter visual mode and spam it easier lol'
+                end,
+                length = 2,
+            },
+            ['%D1[xX]'] = {
+                message = function()
+                    return 'Use literally any deletion motion instead of spamming ts 🥀'
+                end,
+                length = 1,
+            },
+            ['u'] = {
+                message = function()
+                    return 'Use undotree with <leader>u instead of spamming u like a loser'
+                end,
+                length = 1,
+            },
         },
         disabled_keys = {
             ['<C-w>c'] = { 'n' },
