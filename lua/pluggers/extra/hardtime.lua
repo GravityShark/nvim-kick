1aefdf09ef9b87d1401bd59ee00b39fbe992f83d
Gravity <alternativegravitational@protonmail.com>
2025-05-26 17:46:57 +0800

    yo: gurt

--- lua/pluggers/extra/hardtime.lua
+++ lua/pluggers/extra/hardtime.lua
@@ -14,41 +14,42 @@ return {
             ['E'] = { 'n', 'x' },
             ['b'] = { 'n', 'x' },
             ['B'] = { 'n', 'x' },
-            ['J'] = { 'n', 'x' },
-            ['x'] = { 'n', 'x' },
-            ['X'] = { 'n', 'x' },
-            ['<'] = { 'n', 'x' },
-            ['>'] = { 'n', 'x' },
+            -- ['J'] = { n', 'x' },
+            -- ['x'] = { 'n', 'x' },
+            -- ['X'] = { 'n', 'x' },
+            -- ['<'] = { 'n', 'x' },
+            -- ['>'] = { 'n', 'x' },
             ['u'] = { 'n', 'x' },
+            ['<C-r>'] = { 'n', 'x' },
             ['<BS>'] = { 'i' },
         },
-        hints = {
-            ['%D1[webWEB]'] = {
-                message = function()
-                    return 'Use f or t instead of spamming vro 💔'
-                end,
-                length = 1,
-            },
-
-            ['%D1[<>]'] = {
-                message = function()
-                    return 'Enter visual mode and spam it easier lol'
-                end,
-                length = 2,
-            },
-            ['%D1[xX]'] = {
-                message = function()
-                    return 'Use literally any deletion motion instead of spamming ts 🥀'
-                end,
-                length = 1,
-            },
-            ['u'] = {
-                message = function()
-                    return 'Use undotree with <leader>u instead of spamming u like a loser'
-                end,
-                length = 1,
-            },
-        },
+        -- hints = {
+        --     ['%D1[webWEB]'] = {
+        --         message = function()
+        --             return 'Use f or t instead of spamming vro 💔'
+        --         end,
+        --         length = 1,
+        --     },
+        --
+        --     ['%D1[<>]'] = {
+        --         message = function()
+        --             return 'Enter visual mode and spam it easier lol'
+        --         end,
+        --         length = 2,
+        --     },
+        --     ['%D1[xX]'] = {
+        --         message = function()
+        --             return 'Use literally any deletion motion instead of spamming ts 🥀'
+        --         end,
+        --         length = 1,
+        --     },
+        --     ['u'] = {
+        --         message = function()
+        --             return 'Use undotree with <leader>u instead of spamming u like a loser'
+        --         end,
+        --         length = 1,
+        --     },
+        -- },
         disabled_keys = {
             ['<C-w>c'] = { 'n' },
             ['<ESC>'] = { 'n' },
