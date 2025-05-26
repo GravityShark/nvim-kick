6b581e112728255822eaa41768349ea98de5cf3d
Gravity <alternativegravitational@protonmail.com>
2025-05-26 17:32:00 +0800

    better hardtime

--- lua/pluggers/extra/hardtime.lua
+++ lua/pluggers/extra/hardtime.lua
@@ -15,19 +15,39 @@ return {
             ['b'] = { 'n', 'x' },
             ['B'] = { 'n', 'x' },
             ['J'] = { 'n', 'x' },
-            ['c'] = { 'n' },
-            ['C'] = { 'n' },
-            ['d'] = { 'n' },
-            ['x'] = { 'n' },
-            ['X'] = { 'n' },
-            ['y'] = { 'n' },
-            ['Y'] = { 'n' },
-            ['p'] = { 'n' },
-            ['P'] = { 'n' },
-            ['gp'] = { 'n' },
-            ['gP'] = { 'n' },
-            ['<'] = { 'n' },
-            ['>'] = { 'n' },
+            ['d'] = { 'n', 'x' },
+            ['x'] = { 'n', 'x' },
+            ['X'] = { 'n', 'x' },
+            ['<'] = { 'n', 'x' },
+            ['>'] = { 'n', 'x' },
+            ['u'] = { 'n ', 'x' },
+        },
+        hints = {
+            ['[webWEB]'] = {
+                message = function()
+                    return 'Use f or t instead of spamming vro 💔'
+                end,
+                length = 2,
+            },
+
+            ['[<>]'] = {
+                message = function()
+                    return 'Enter visual mode and spam it easier lol'
+                end,
+                length = 2,
+            },
+            ['[xXd]'] = {
+                message = function()
+                    return 'Use literally any deletion motion instead of spamming ts 🥀'
+                end,
+                length = 2,
+            },
+            ['u'] = {
+                message = function()
+                    return 'Use <leader>u instead of spamming u like a loser'
+                end,
+                length = 2,
+            },
         },
         disabled_keys = {
             ['<C-w>c'] = { 'n' },
