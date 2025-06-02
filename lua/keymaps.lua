e701e9f0e4bd9c18de9849fa2751c11ebe51f165
Gravity <alternativegravitational@protonmail.com>
2025-06-02 11:25:35 +0800

    yeah

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -232,7 +232,7 @@ vim.api.nvim_set_keymap('n', ']q', '<CMD>cnext<CR>', { desc = 'Next quickfix' })
 
 -- }}}
 -- plus and minus for decrement and increment {{{
-vim.api.nvim_set_keymap('n', '+', '<C-a>', { desc = 'Increment' })
-vim.api.nvim_set_keymap('n', '-', '<C-x>', { desc = 'Decrement' })
+-- vim.api.nvim_set_keymap('n', '+', '<C-a>', { desc = 'Increment' })
+-- vim.api.nvim_set_keymap('n', '-', '<C-x>', { desc = 'Decrement' })
 -- }}}
 -- vim:foldmethod=marker:
