1c77e3e320439c6363133a41cf085e22bf3816a5
Gravity <alternativegravitational@protonmail.com>
2025-06-08 18:01:39 +0800

    guess indent keybind

--- lua/pluggers/editor/guess-indent.lua
+++ lua/pluggers/editor/guess-indent.lua
@@ -1,5 +1,7 @@
 return { -- guess-indent.nvim Automatically guess the indent level
     'nmac427/guess-indent.nvim',
+    keys = { '<Leader>cg', '<CMD>GuessIndent<CR>', desc = 'Guess Indent' },
+    cmd = 'GuessIndent',
     opts = {},
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
 }
