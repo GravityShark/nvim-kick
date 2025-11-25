54cf2b28d2c71b5a369edb45eee5774cf5175a56
Gravity <alternativegravitational@protonmail.com>
2025-11-26 07:12:20 +0800

    oiling uip with some images

--- lua/pluggers/extra/oil.lua
+++ lua/pluggers/extra/oil.lua
@@ -4,7 +4,18 @@ return {
     keys = {
         { '<leader>.', '<CMD>Oil --float<CR>', desc = 'file manager' },
     },
-    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    dependencies = {
+        { 'nvim-mini/mini.icons', opts = {} },
+        { 'JezerM/oil-lsp-diagnostics.nvim', opts = {} },
+        { 'JezerM/oil-lsp-diagnostics.nvim', opts = {} },
+        {
+            '3rd/image.nvim',
+            build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+            opts = {
+                processor = 'magick_cli',
+            },
+        },
+    },
     event = {
         {
             { 'OilEnter' },
@@ -22,32 +33,27 @@ return {
     opts = {
         default_file_explorer = true,
         delete_to_trash = true,
-        columns = { 'icon', 'mtime' },
         skip_confirm_for_simple_edits = true,
         lsp_file_methods = { autosave_changes = true },
         watch_for_changes = true,
         keymaps = {
             ['<leader>.'] = { 'actions.close', mode = 'n' },
-            ['<S-CR>'] = function()
-                local oil = require('oil')
-                local actions = require('oil.actions')
-
-                local entry = oil.get_cursor_entry()
-                if not entry then
-                    return
-                end
-
-                local name = entry.name
-                local path = oil.get_current_dir() .. name
-
-                -- handle directories
-                local fs = vim.loop.fs_stat(path)
-                if fs and fs.type == 'directory' then
-                    return oil.select({ vertical = false })
-                end
-
-                actions.open_external.callback()
-            end,
+            ['gd'] = {
+                desc = 'Toggle file detail view',
+                callback = function()
+                    detail = not detail
+                    if detail then
+                        require('oil').set_columns({
+                            'icon',
+                            'permissions',
+                            'size',
+                            'mtime',
+                        })
+                    else
+                        require('oil').set_columns({ 'icon' })
+                    end
+                end,
+            },
         },
         float = { border = 'rounded' },
     },
