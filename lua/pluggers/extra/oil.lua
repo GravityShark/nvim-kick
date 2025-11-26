bb405526119aac04a9eb7d242424432c9ef93e37
Gravity <alternativegravitational@protonmail.com>
2025-11-26 09:14:47 +0800

    asdfaj;sdf

--- lua/pluggers/extra/oil.lua
+++ lua/pluggers/extra/oil.lua
@@ -32,6 +32,17 @@ return {
             build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
             opts = {
                 processor = 'magick_cli',
+                window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+                window_overlap_clear_ft_ignore = {
+                    'cmp_menu',
+                    'cmp_docs',
+                    'snacks_notif',
+                    'scrollview',
+                    'scrollview_sign',
+                    'TelescopePrompt',
+                },
+                editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+                tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
             },
         },
     },
