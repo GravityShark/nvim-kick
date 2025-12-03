d5d0579f2434539db864aea496869e63b959227e
Gravity <alternativegravitational@protonmail.com>
2025-12-03 08:21:15 +0800

    fyler be breaking

--- lua/pluggers/extra/fyler.lua
+++ lua/pluggers/extra/fyler.lua
@@ -16,29 +16,32 @@ return {
     branch = 'stable',
     lazy = false, -- Necessary for `default_explorer` to work properly
     opts = {
+        integrations = {
+            icon = 'none',
+        },
         views = {
             finder = {
                 close_on_select = false,
                 default_explorer = true,
                 delete_to_trash = true,
-                -- git_status = {
-                --     symbols = {
-                --         Added = '▎',
-                --         Modified = '▎',
-                --         Deleted = '󰐊',
-                --         topdelete = '󰐊',
-                --         Renamed = '▎',
-                --         Untracked = '┆',
-                --     },
-                -- },
+                git_status = {
+                    symbols = {
+                        Added = '▎',
+                        Modified = '▎',
+                        Deleted = '󰐊',
+                        topdelete = '󰐊',
+                        Renamed = '▎',
+                        Untracked = '┆',
+                    },
+                },
             },
             mappings = {
                 ['q'] = 'CloseView',
                 ['<CR>'] = 'Select',
                 ['<C-t>'] = 'SelectTab',
                 ['|'] = 'SelectVSplit',
-                ['-'] = 'SelectSplit',
-                ['^'] = 'GotoParent',
+                -- ['-'] = 'SelectSplit',
+                ['-'] = 'GotoParent',
                 ['='] = 'GotoCwd',
                 ['.'] = 'GotoNode',
                 ['#'] = 'CollapseAll',
