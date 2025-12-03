a4723e920befa1a91ca7c837be3717adad219fa1
Gravity <alternativegravitational@protonmail.com>
2025-12-03 09:23:53 +0800

    scrollin

--- lua/pluggers/nvim-scrollbar.lua
+++ lua/pluggers/nvim-scrollbar.lua
@@ -2,19 +2,29 @@ return {
     'petertriho/nvim-scrollbar',
     event = 'VeryLazy',
     dependencies = {
-        {
-            'kevinhwang91/nvim-hlslens',
-            config = function()
-                require('scrollbar.handlers.search').setup({})
-            end,
-        },
-        {
-            'lewis6991/gitsigns.nvim',
-            config = function()
-                require('gitsigns').setup()
-                require('scrollbar.handlers.gitsigns').setup()
-            end,
-        },
+        'kevinhwang91/nvim-hlslens',
+        'lewis6991/gitsigns.nvim',
+        -- {
+        --     'kevinhwang91/nvim-hlslens',
+        --     config = function()
+        --         require('scrollbar.handlers.search').setup({})
+        --     end,
+        -- },
+        -- {
+        --     'lewis6991/gitsigns.nvim',
+        --     config = function()
+        --         require('gitsigns').setup()
+        --         require('scrollbar.handlers.gitsigns').setup()
+        --     end,
+        -- },
     },
-    opts = {},
+    opts = {
+        hide_if_all_visible = true,
+        handle = {
+            blend = 0,
+            -- highlight = 'MiniStatuslineModeNormal',
+        },
+        marks = { Cursor = { text = '·' } },
+        handlers = { gitsigns = true, search = true },
+    },
 }
