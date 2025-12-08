c6f5440934a561d9f771ff39a711861d5a1b6e1d
Gravity <alternativegravitational@protonmail.com>
2025-12-08 16:03:14 +0800

    use

---
+++ lua/pluggers/colors/gruvbox-material.lua
@@ -0,0 +1,18 @@
+return { -- soho vibes
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+        vim.opt.background = 'light'
+    end,
+    -- opts = {
+    --     highlight_groups = {
+    --         Folded = { bg = 'base' },
+    --         MiniJump = { fg = 'subtle', undercurl = false, underline = true },
+    --     },
+    -- },
+}
