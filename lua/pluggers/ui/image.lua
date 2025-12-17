3a4fe06a0cdc75a5ccfec08934a9a52d78e8d203
Gravity <alternativegravitational@protonmail.com>
2025-12-17 10:23:34 +0800

    try

--- lua/pluggers/ui/image.lua
+++ lua/pluggers/ui/image.lua
@@ -4,16 +4,30 @@ return {
     event = 'VeryLazy',
     opts = {
         processor = 'magick_cli',
-        -- window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-        -- window_overlap_clear_ft_ignore = {
-        --     'cmp_menu',
-        --     'cmp_docs',
-        --     'snacks_notif',
-        --     'scrollview',
-        --     'scrollview_sign',
-        --     'TelescopePrompt',
-        -- },
+        window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+        window_overlap_clear_ft_ignore = {
+            'cmp_menu',
+            'cmp_docs',
+            'snacks_notif',
+            'scrollview',
+            'scrollview_sign',
+            'TelescopePrompt',
+        },
         editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
         tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    },
+    hijack_file_patterns = {
+        '*.png',
+        '*.jpg',
+        '*.jpeg',
+        '*.gif',
+        '*.bmp',
+        '*.heic',
+        '*.xpm',
+        '*.ico',
+        '*.svg',
+        '*.webp',
+        '*.avif',
+        '*.pdf',
     },
 }
