3c0779ca6c50096443c9224e14258722c53a27db
Gravity <alternativegravitational@protonmail.com>
2025-12-20 09:53:18 +0800

    fixin up

--- lua/pluggers/extra/which-key.lua
+++ lua/pluggers/extra/which-key.lua
@@ -1,21 +1,10 @@
 return { -- which-key.nvim Keymap hints for the slow
     'folke/which-key.nvim',
     event = 'VeryLazy',
-    config = function()
-        local wk = require('which-key')
-        wk.setup({
-            -- delay = 0,
-            win = { no_overlap = false },
-            icons = { separator = '', group = '', rules = false },
-            sort = { 'alphanum ' },
-        })
-        -- wk.add({
-        --     { '[a', desc = 'Previous argument' },
-        --     { ']a', desc = 'Next argument' },
-        --     { '[c', desc = 'Previous class' },
-        --     { ']c', desc = 'Next class' },
-        --     { '[f', desc = 'Previous function' },
-        --     { ']f', desc = 'Next function' },
-        -- })
-    end,
+    opts = {
+        preset = 'helix',
+        delay = 0,
+        icons = { separator = '', group = '', rules = false },
+        sort = { 'alphanum ' },
+    },
 }
