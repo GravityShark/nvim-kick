fbb87cd39cf6184f765cf758f23e77f9fa537536
Gravity <alternativegravitational@protonmail.com>
2025-12-20 17:47:55 +0800

    type

--- lua/pluggers/colors/gruvbox-material.lua
+++ lua/pluggers/colors/gruvbox-material.lua
@@ -2,10 +2,9 @@ return { -- gruvvin
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
-    init = function()
+    config = function()
         vim.g.gruvbox_material_enable_italic = true
         vim.cmd.colorscheme('gruvbox-material')
         vim.opt.background = 'light'
     end,
-    opts = true,
 }
