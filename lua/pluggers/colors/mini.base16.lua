a8e27c8d60d6897aecc5c2d2eb151fac1746e5dc
Gravity <alternativegravitational@protonmail.com>
2025-12-24 00:28:59 +0800

    yoyoyo

--- lua/pluggers/colors/mini.base16.lua
+++ lua/pluggers/colors/mini.base16.lua
@@ -4,5 +4,9 @@ return {
     priority = 1000,
     init = function()
         vim.cmd.colorscheme('base16')
+        vim.cmd.highlight({ 'SignColumn', 'guibg=NONE', 'ctermbg=NONE' })
+        vim.cmd.highlight({ 'LineNr', 'guibg=NONE', 'ctermbg=NONE' })
+        vim.cmd.highlight({ 'LineNrAbove', 'guibg=NONE', 'ctermbg=NONE' })
+        vim.cmd.highlight({ 'LineNrBelow', 'guibg=NONE', 'ctermbg=NONE' })
     end,
 }
