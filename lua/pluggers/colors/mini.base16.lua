796bcf7c973c31fa704d4f42d28d1b5810397402
Gravity <alternativegravitational@protonmail.com>
2025-12-23 22:11:40 +0800

    yarrr

--- lua/pluggers/colors/mini.base16.lua
+++ lua/pluggers/colors/mini.base16.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-        palette = require('base16'),
+        palette = require(vim.fn.stdpath('data') .. 'base16.lua'),
         use_cterm = true,
         plugins = { default = false },
     },
