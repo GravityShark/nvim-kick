c2faea4ed1cacfb4e8481db0abfd2890f2f12ee8
Gravity <alternativegravitational@protonmail.com>
2025-12-25 07:59:10 +0800

    gruvvin

--- lua/pluggers/colors/gruvbox-material.lua
+++ lua/pluggers/colors/gruvbox-material.lua
@@ -26,10 +26,25 @@ return { -- gruvvin
     priority = 1000,
     config = function()
         vim.g.gruvbox_material_enable_italic = '1'
-        vim.g.gruvbox_material_better_performance = '1'
-        -- vim.g.gruvbox_material_diagnostic_text_highlight = true
-        vim.cmd.colorscheme('gruvbox-material')
+        -- vim.g.gruvbox_material_better_performance = '1'
+        vim.api.nvim_create_autocmd('ColorScheme', {
+            group = vim.api.nvim_create_augroup(
+                'custom_highlights_gruvboxmaterial',
+                {}
+            ),
+            pattern = 'gruvbox-material',
+            callback = function()
+                local config = vim.fn['gruvbox_material#get_configuration']()
+                local palette = vim.fn['gruvbox_material#get_palette'](
+                    config.background,
+                    config.foreground,
+                    config.colors_override
+                )
+                local set_hl = vim.fn['gruvbox_material#highlight']
 
-        vim.api.nvim_set_hl(0, 'Folded', { bg = 'NONE' })
+                set_hl('Search', palette.none, {})
+            end,
+        })
+        vim.cmd.colorscheme('gruvbox-material')
     end,
 }
