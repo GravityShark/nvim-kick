0b491c10d18538d9d84d94a20eca58643fd7eaff
Gravity <alternativegravitational@protonmail.com>
2025-12-25 12:32:25 +0800

    yup

--- lua/pluggers/colors/gruvbox-material.lua
+++ lua/pluggers/colors/gruvbox-material.lua
@@ -24,7 +24,7 @@ return { -- gruvvin
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
-    config = function()
+    init = function()
         vim.g.gruvbox_material_enable_italic = 1
         vim.g.gruvbox_material_better_performance = 1
         vim.cmd.colorscheme('gruvbox-material')
--- lua/pluggers/ui/neominimap.lua
+++ lua/pluggers/ui/neominimap.lua
@@ -1,7 +1,7 @@
 return {
     'Isrothy/neominimap.nvim',
     version = 'v3.x.x',
-    lazy = false, -- NOTE: NO NEED to Lazy load
+    lazy = false,
     keys = {
         -- Global Minimap Controls
         {
