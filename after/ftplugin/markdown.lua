25855c97f725c1dfcf92b5ee674770df1ec7a7e3
Gravity <alternativegravitational@protonmail.com>
2025-12-31 11:48:09 +0800

    ayo chill

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,3 @@
-vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt_local.foldtext = 'v:lua.vim.treesitter.foldtext()'
-vim.opt_local.foldmethod = 'expr'
+-- vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- vim.opt_local.foldtext = 'v:lua.vim.treesitter.foldtext()'
+-- vim.opt_local.foldmethod = 'expr'
