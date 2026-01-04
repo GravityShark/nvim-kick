80c0af20acf9aabe3853fc0abc713915809b30b8
Gravity <alternativegravitational@protonmail.com>
2026-01-05 06:43:07 +0800

    make obsidian kinda better

--- lua/pluggers/extra/obsidian.lua
+++ lua/pluggers/extra/obsidian.lua
@@ -10,16 +10,54 @@ return {
         {
             '<leader>od',
             '<CMD>Obsidian dailies<CR>',
-            desc = 'daily',
+            desc = 'daily picker',
         },
+        {
+            '<leader>on',
+            '<CMD>Obsidian new<CR>',
+            desc = 'new',
+        },
+        {
+            '<leader>ot',
+            '<CMD>Obsidian today<CR>',
+            desc = 'today',
+        },
+        {
+            '<leader>of',
+            '<CMD>Obsidian quick_switch<CR>',
+            desc = 'find notes',
+        },
+        {
+            '<leader>of',
+            '<CMD>Obsidian search<CR>',
+            desc = 'Find text',
+        },
+        {
+            '<leader>op',
+            '<CMD>Obsidian paste_img<CR>',
+            desc = 'paste image',
+        },
+        {
+            '<leader>o/',
+            '<CMD>Obsidian<CR>',
+            desc = 'Obsidian',
+        },
+    },
+    dependencies = {
+        { 'bullets-vim/bullets.vim' },
+        -- https://github.com/Myzel394/easytables.nvim
+        -- https://github.com/HakonHarnes/img-clip.nvim
+        -- https://github.com/Thiago4532/mdmath.nvim
+        -- https://github.com/artempyanykh/marksman
+        -- https://github.com/Myzel394/easytables.nvim
     },
     opts = {
         daily_notes = { folder = '/0-inbox' },
-        -- new_notes_location = 'Valuts/0-inbox',
+        legacy_commands = false, -- this will be removed in the next major release
         notes_subdir = '/0-inbox',
         new_notes_location = 'notes_subdir',
         note_id_func = function(id)
-            return id
+            return id and id or ''
         end,
         templates = { folder = '/templates' },
         workspaces = {
