7896833eda54b4d8d6f20c7e9b32f2087fb54c65
Gravity <alternativegravitational@protonmail.com>
2026-01-05 06:12:38 +0800

    obby

--- lua/pluggers/extra/obsidian.lua
+++ lua/pluggers/extra/obsidian.lua
@@ -15,7 +15,9 @@ return {
     },
     opts = {
         daily_notes = { folder = '/0-inbox' },
-        new_notes_location = '/0-inbox',
+        -- new_notes_location = 'Valuts/0-inbox',
+        notes_subdir = '/0-inbox',
+        new_notes_location = 'notes_subdir',
         note_id_func = function(id)
             return id
         end,
