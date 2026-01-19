bb9dfd0cc71410309944169f842dd6d349a8c521
Gravity <alternativegravitational@protonmail.com>
2026-01-20 06:43:32 +0800

    oops

--- lua/pluggers/extra/live-preview.lua
+++ lua/pluggers/extra/live-preview.lua
@@ -2,7 +2,7 @@ return {
     -- 'brianhuster/live-preview.nvim',
     -- cmd = 'LivePreview',
 
-    'IllicitFrog/html_preview.nvim',
+    'IllicitFrog/html-preview.nvim',
     ft = 'html',
     cmd = 'HtmlPreview',
     opts = {},
