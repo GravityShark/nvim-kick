9522f0f8060f4c3b8367b3475a2cda9eb0bd504d
Gravity <alternativegravitational@protonmail.com>
2026-01-20 07:48:19 +0800

    yo

--- lua/pluggers/extra/oil.lua
+++ lua/pluggers/extra/oil.lua
@@ -23,44 +23,61 @@ return {
 
     config = function()
         local oil = require('oil')
-        local visualler = function(impl)
-            return function()
-                -- https://github.com/asdf8601/kickstart.nvim/blob/687d4da63f4befcdd3c6c1e1e0f375b449984f18/lua/kickstart/plugins/oil-file-manager.lua#L1-L41
-                -- Open cmdline with visually selected entries as argument
-                -- : <file1> <file2> <file..>
-                local mode = string.lower(vim.api.nvim_get_mode().mode)
-                local bufnr = vim.api.nvim_get_current_buf()
-                local name
-                local cwd = oil.get_current_dir()
-                local items = {}
+        local get_oil_selection = function()
+            -- https://github.com/asdf8601/kickstart.nvim/blob/687d4da63f4befcdd3c6c1e1e0f375b449984f18/lua/kickstart/plugins/oil-file-manager.lua#L1-L41
+            -- https://github.com/stevearc/conform.nvim/blob/62d5accad8b29d6ba9b58d3dff90c43a55621c60/lua/conform/init.lua#L324-L353
+            local mode = vim.api.nvim_get_mode().mode
+            local bufnr = vim.api.nvim_get_current_buf()
+            local start = vim.fn.getpos('v')
+            local end_ = vim.fn.getpos('.')
+            local start_row = start[2]
+            local start_col = start[3]
+            local end_row = end_[2]
+            local end_col = end_[3]
 
-                if mode == 'n' then
-                    local lnum = vim.fn.getpos('.')[2]
-                    name = oil.get_entry_on_line(bufnr, lnum).name
-                    table.insert(items, cwd .. name)
-                elseif mode == 'v' then
-                    local start = vim.fn.getpos('v')
-                    local end_ = vim.fn.getpos('.')
-                    local lnum0 = start[2]
-                    local lnum1 = end_[2]
-                    print(lnum0, lnum1)
-                    for lnum = lnum0, lnum1 do
-                        _ = oil.get_entry_on_line(bufnr, lnum)
-                        name = oil.get_entry_on_line(bufnr, lnum).name
-                        table.insert(items, cwd .. name)
-                    end
-                end
-                impl(items, mode)
+            -- A user can start visual selection at the end and move backwards
+            -- Normalize the range to start < end
+            if start_row == end_row and end_col < start_col then
+                end_col, start_col = start_col, end_col
+            elseif end_row < start_row then
+                start_row, end_row = end_row, start_row
+                start_col, end_col = end_col, start_col
             end
+            if mode == 'V' then
+                start_col = 1
+                local lines = vim.api.nvim_buf_get_lines(
+                    bufnr,
+                    end_row - 1,
+                    end_row,
+                    true
+                )
+                end_col = #lines[1]
+            end
+
+            local items = {}
+
+            for lnum = start_row, end_row do
+                _ = oil.get_entry_on_line(bufnr, lnum)
+                table.insert(
+                    items,
+                    oil.get_current_dir()
+                        .. oil.get_entry_on_line(bufnr, lnum).name
+                )
+            end
+
+            return items
         end
 
-        local function open_cmdline_with_path(paths, m)
+        local function open_cmdline_with_path()
+            local m = vim.api.nvim_get_mode().mode
+            local paths = get_oil_selection()
+
             local rm = ''
             local args = ''
             for _, path in ipairs(paths) do
                 args = args .. ' ' .. vim.fn.fnameescape(path)
             end
-            if m == 'v' then
+            if m == 'v' or m == 'V' then
                 rm = '<Del><Del><Del><Del><Del>'
             end
             local escaped = vim.api.nvim_replace_termcodes(
@@ -72,7 +89,9 @@ return {
             vim.api.nvim_feedkeys(escaped, m, true)
         end
 
-        local function open_file_with_path(paths, _)
+        local function open_file_with_path()
+            local paths = get_oil_selection()
+
             for _, path in ipairs(paths) do
                 vim.ui.open(path)
             end
@@ -92,7 +111,6 @@ return {
             preview_win = { preview_method = 'scratch' },
             keymaps = {
                 ['<ESC>'] = { 'actions.close', mode = 'n' },
-                ['<S-CR>'] = { 'actions.open_external', mode = 'n' },
                 ['gp'] = { 'actions.preview', mode = 'n' },
                 ['gd'] = {
                     desc = 'Toggle file detail view',
@@ -111,12 +129,14 @@ return {
                     end,
                 },
                 ['g!'] = {
-                    visualler(open_cmdline_with_path),
+                    open_cmdline_with_path,
                     desc = 'Open selected item on cmdline',
+                    mode = 'v',
                 },
-                ['gX'] = {
-                    visualler(open_file_with_path),
+                ['gx'] = {
+                    open_file_with_path,
                     desc = 'Open selected item on external program',
+                    mode = 'v',
                 },
                 ['g1'] = {
                     'actions.open_cmdline',
