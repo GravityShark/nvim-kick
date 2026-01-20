181d832f9019a570ad166b0722b3391996d4b373
Gravity <alternativegravitational@protonmail.com>
2026-01-20 22:19:58 +0800

    frick

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,10 +1,29 @@
 -- https://github.com/linkarzu/dotfiles-latest/blob/990d77fbc13ea506968c2ce1617def110c6c02ad/neovim/neobean/lua/config/keymaps.lua#L3104C1-L3126C4
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
+function _G.markdown_foldexpr()
+    local lnum = vim.v.lnum
+    local line = vim.fn.getline(lnum)
+    local heading = line:match('^(#+)%s')
+    if heading then
+        local level = #heading
+        if level == 1 then
+            -- Special handling for H1
+            if lnum == 1 then
+                return '>1'
+            else
+                local frontmatter_end = vim.b.frontmatter_end
+                if frontmatter_end and (lnum == frontmatter_end + 1) then
+                    return '>1'
+                end
+            end
+        elseif level >= 2 and level <= 6 then
+            -- Regular handling for H2-H6
+            return '>' .. level
+        end
+    end
+    return '='
+end
 
--- vim.opt_local.foldexpr = 'v:lua.markdown_foldexpr()'
--- vim.opt_local.foldtext = 'v:lua.vim.treesitter.foldtext()'
-
+-- https://github.com/linkarzu/dotfiles-latest/blob/990d77fbc13ea506968c2ce1617def110c6c02ad/neovim/neobean/lua/config/keymaps.lua#L3104C1-L3126C4
 -- Detect frontmatter closing line
 local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 local found_first = false
@@ -19,4 +38,7 @@ for i, line in ipairs(lines) do
         end
     end
 end
+
 vim.b.frontmatter_end = frontmatter_end
+vim.opt_local.foldexpr = 'v:lua.markdown_foldexpr()'
+vim.opt_local.foldtext = 'v:lua.vim.treesitter.foldtext()'
--- lua/config/foldtext.lua
+++ lua/config/foldtext.lua
@@ -101,8 +101,6 @@ function HighlightedFoldtext()
     local result = parse_line(vim.v.foldstart)
     if not result then
         return vim.fn.foldtext()
-    elseif vim.bo.filetype == 'markdown' then
-        return result
     end
 
     local folded = {
@@ -148,28 +146,3 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 })
 
 vim.opt.foldtext = 'luaeval("HighlightedFoldtext")()'
-
--- https://github.com/linkarzu/dotfiles-latest/blob/990d77fbc13ea506968c2ce1617def110c6c02ad/neovim/neobean/lua/config/keymaps.lua#L3104C1-L3126C4
-function _G.markdown_foldexpr()
-    local lnum = vim.v.lnum
-    local line = vim.fn.getline(lnum)
-    local heading = line:match('^(#+)%s')
-    if heading then
-        local level = #heading
-        if level == 1 then
-            -- Special handling for H1
-            if lnum == 1 then
-                return '>1'
-            else
-                local frontmatter_end = vim.b.frontmatter_end
-                if frontmatter_end and (lnum == frontmatter_end + 1) then
-                    return '>1'
-                end
-            end
-        elseif level >= 2 and level <= 6 then
-            -- Regular handling for H2-H6
-            return '>' .. level
-        end
-    end
-    return '='
-end
