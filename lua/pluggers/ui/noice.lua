3acc081e8e798583949760c68003a9bff4c26acc
Gravity <alternativegravitational@protonmail.com>
2026-01-20 09:04:43 +0800

    gappl

--- lua/pluggers/ui/noice.lua
+++ lua/pluggers/ui/noice.lua
@@ -28,28 +28,38 @@ return {
             },
         },
         routes = {
+
             {
-                -- https://github.com/folke/noice.nvim/issues/1097
+                filter = { event = 'msg_show' },
                 view = 'notify',
-                filter = {
-                    event = 'msg_show',
-                    kind = {
-                        'shell_out',
-                        'shell_err',
-                    },
+                opts = {
+                    level = 'info',
+                    skip = false,
+                    replace = false,
                 },
             },
-            {
-                filter = {
-                    event = 'msg_show',
-                    any = {
-                        { find = '%d+L, %d+B' },
-                        { find = '; after #%d+' },
-                        { find = '; before #%d+' },
-                    },
-                },
-                view = 'mini',
-            },
+            -- {
+            --     -- https://github.com/folke/noice.nvim/issues/1097
+            --     view = 'notify',
+            --     filter = {
+            --         event = 'msg_show',
+            --         kind = {
+            --             'shell_out',
+            --             'shell_err',
+            --         },
+            --     },
+            -- },
+            -- {
+            --     view = 'mini',
+            --     filter = {
+            --         event = 'msg_show',
+            --         any = {
+            --             { find = '%d+L, %d+B' },
+            --             { find = '; after #%d+' },
+            --             { find = '; before #%d+' },
+            --         },
+            --     },
+            -- },
         },
         presets = {
             bottom_search = true,
