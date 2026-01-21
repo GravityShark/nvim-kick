35cd054ace9b4dcf6401917a5aebec826a10490f
Gravity <alternativegravitational@protonmail.com>
2026-01-21 10:10:08 +0800

    yeah

--- lua/pluggers/extra/bullets.lua
+++ lua/pluggers/extra/bullets.lua
@@ -1,6 +1,7 @@
 return {
     'bullets-vim/bullets.vim',
-    events = { 'InsertEnter', 'VeryLazy' },
+    ft = 'markdown',
+    event = { 'InsertEnter', 'VeryLazy' },
     init = function()
         vim.g.bullets_outline_levels = { 'num', 'abc' }
     end,
