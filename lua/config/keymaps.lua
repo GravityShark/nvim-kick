d06b83d1eacc57943e539126177ebbca8389afd3
Gravity <alternativegravitational@protonmail.com>
2026-02-01 11:14:29 +0800

    yuh

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,10 +10,10 @@ vim.api.nvim_set_keymap('t', '<C-space>', '<C-\\><C-n>', { silent = true })
 -- Enter terminal
 vim.api.nvim_set_keymap(
     'n',
-    '<leader>S',
+    '<leader>t',
     -- '<CMD>split term://$SHELL<CR><CMD>set nu!<CR><CMD>set rnu!<CR>i',
     '<CMD>e term://$SHELL<CR><CMD>set nu!<CR><CMD>set rnu!<CR>',
-    { desc = 'Shell' }
+    { desc = 'terminal' }
 )
 -- }}}
 
